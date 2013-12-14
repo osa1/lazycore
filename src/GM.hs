cd3e369b207d568b94f59a80144d2770a79d3429
osa1 <omeragacan@gmail.com>
2013-12-14 10:36:09 +0200

    buggy implementation of mark 4

--- src/GM.hs
+++ src/GM.hs
@@ -26,20 +26,33 @@ parse s = case P.parse program s s of
             Left err -> error (show err)
             Right p -> p
 
-type GmState
-    = (GmCode,      -- Current instruction stream
-       GmStack,     -- Current stack
-       GmHeap,      -- Heap of nodes
-       GmGlobals,   -- Global addresses in heap
-       GmStats)     -- Statistics
+--
+-- Types
+--
+
+data GmState = GmState
+    { getCode :: GmCode         -- Current instruction stream
+    , getStack :: GmStack       -- Current stack
+    , getDump :: GmDump         -- Current dump
+    , getHeap :: GmHeap         -- Heap of nodes
+    , getGlobals :: GmGlobals   -- Global addresses in heap
+    , getStats :: GmStats       -- Statistics
+    } deriving ( Show )
 
 type GmCode = [Instruction]
+type GmStack = [Addr]
+type GmHeap = Heap Node
+type GmDump = [GmDumpItem]
+type GmDumpItem = (GmCode, GmStack)
+type GmGlobals = M.Map Name Addr
+type GmStats = Int
 
-getCode :: GmState -> GmCode
-getCode (i, _, _, _, _) = i
-
-putCode :: GmCode -> GmState -> GmState
-putCode i (_, stack, heap, globals, stats) = (i, stack, heap, globals, stats)
+data Node
+    = NNum Int              -- Numbers
+    | NAp Addr Addr         -- Applications
+    | NGlobal Int GmCode    -- Globals
+    | NInd Addr             -- Indirections
+    deriving ( Show, Eq )
 
 data Instruction
     = Unwind
@@ -51,37 +64,27 @@ data Instruction
     | Pop Int
     | Alloc Int
     | Slide Int
+    | Eval
+    | Add | Sub | Mul | Div | Neg
+    | Eq | Ne | Lt | Le | Gt | Ge
+    | Cond GmCode GmCode
     deriving ( Show, Eq )
 
-type GmStack = [Addr]
+--
+-- Setters
+--
 
-getStack :: GmState -> GmStack
-getStack (_, stack, _, _, _) = stack
+putCode :: GmCode -> GmState -> GmState
+putCode i state = state{getCode = i}
 
 putStack :: GmStack -> GmState -> GmState
-putStack stack (i, _, heap, globals, stats) = (i, stack, heap, globals, stats)
-
-type GmHeap = Heap Node
-
-getHeap :: GmState -> GmHeap
-getHeap (_, _, heap, _, _) = heap
+putStack stack state = state{getStack = stack}
 
 putHeap :: GmHeap -> GmState -> GmState
-putHeap heap (i, stack, _, globals, stats) = (i, stack, heap, globals, stats)
+putHeap heap state = state{getHeap = heap}
 
-data Node
-    = NNum Int              -- Numbers
-    | NAp Addr Addr         -- Applications
-    | NGlobal Int GmCode    -- Globals
-    | NInd Addr             -- Indirections
-    deriving ( Show, Eq )
-
-type GmGlobals = M.Map Name Addr
-
-getGlobals :: GmState -> GmGlobals
-getGlobals (_, _, _, globals, _) = globals
-
-type GmStats = Int
+putDump :: GmDump -> GmState -> GmState
+putDump dump state = state{getDump = dump}
 
 statInitial :: GmStats
 statInitial = 0
@@ -92,11 +95,8 @@ statIncStep = (+) 1
 statGetSteps :: GmStats -> Int
 statGetSteps = id
 
-getStats :: GmState -> GmStats
-getStats (_, _, _, _, stats) = stats
-
 putStats :: GmStats -> GmState -> GmState
-putStats stats (i, stack, heap, globals, _) = (i, stack, heap, globals, stats)
+putStats stats state = state{getStats = stats}
 
 
 --------------------------------------------------------------------------------
@@ -107,7 +107,7 @@ putStats stats (i, stack, heap, globals, _) = (i, stack, heap, globals, stats)
 --
 
 compile :: CoreProgram -> GmState
-compile pgm = (initialCode, [], heap, globals, statInitial)
+compile pgm = GmState initialCode [] [] heap globals statInitial
   where (heap, globals) = buildInitialHeap compiled hInitial M.empty
 
         buildInitialHeap :: [GmCompiledSC] -> GmHeap -> GmGlobals -> (GmHeap, GmGlobals)
@@ -122,14 +122,26 @@ compile pgm = (initialCode, [], heap, globals, statInitial)
 type GmCompiledSC = (Name, Int, GmCode)
 
 compiledPrimitives :: [GmCompiledSC]
-compiledPrimitives = []
+compiledPrimitives =
+    [ ("+", 2, [Push 1, Eval, Push 1, Eval, Add, Update 2, Pop 2, Unwind])
+    , ("-", 2, [Push 1, Eval, Push 1, Eval, Sub, Update 2, Pop 2, Unwind])
+    , ("*", 2, [Push 1, Eval, Push 1, Eval, Mul, Update 2, Pop 2, Unwind])
+    , ("/", 2, [Push 1, Eval, Push 1, Eval, Div, Update 2, Pop 2, Unwind])
+    , ("negate", 1, [Push 0, Eval, Neg, Update 1, Pop 1, Unwind])
+    , ("==", 2, [Push 1, Eval, Push 1, Eval, Eq, Update 2, Pop 2, Unwind])
+    , ("~=", 2, [Push 1, Eval, Push 1, Eval, Ne, Update 2, Pop 2, Unwind])
+    , ("<", 2, [Push 1, Eval, Push 1, Eval, Lt, Update 2, Pop 2, Unwind])
+    , ("<=", 2, [Push 1, Eval, Push 1, Eval, Le, Update 2, Pop 2, Unwind])
+    , (">=", 2, [Push 1, Eval, Push 1, Eval, Ge, Update 2, Pop 2, Unwind])
+    , ("if", 3, [Push 0, Eval, Cond [Push 1] [Push 2], Update 3, Pop 3, Unwind])
+    ]
 
 allocateSc :: GmHeap -> GmCompiledSC -> (GmHeap, (Name, Addr))
 allocateSc heap (name, nargs, instns) = (heap', (name, addr))
   where (heap', addr) = hAlloc heap (NGlobal nargs instns)
 
 initialCode :: GmCode
-initialCode = [Pushglobal "main", Unwind]
+initialCode = [Pushglobal "main", Eval]
 
 compileSc :: (Name, [Name], CoreExpr) -> GmCompiledSC
 compileSc (name, env, body) = (name, length env, compileR body (M.fromList $ zip env [0..]))
@@ -231,25 +243,27 @@ dispatch (Update n) state = putStack as (putHeap (hUpdate heap (as !! n) (NInd a
 dispatch (Pop n) state = putStack (drop n (getStack state)) state
 
 dispatch Unwind state = newState (hLookup heap a)
-  where stack@(a : as) = getStack state
-        heap           = getHeap state
+  where
+    stack@(a : as) = getStack state
+    heap           = getHeap state
+    ((i, s) : ds)  = getDump state
 
-        newState (NInd n) = putCode [Unwind] (putStack (n : as) state)
-        newState NNum{} = state
-        newState (NAp a1 _) = putCode [Unwind] (putStack (a1 : a : as) state)
-        newState (NGlobal n c)
-          | length as < n = error $
-              concat [ "Unwinding with too few arguments. "
-                     , show n, " - ", show c ]
-          | otherwise     = putCode c (putStack (rearrange n heap stack) state)
+    newState (NInd n) = putCode [Unwind] (putStack (n : as) state)
+    newState NNum{}   = putCode i (putStack (a : s) (putDump ds state))
+    newState (NAp a1 _) = putCode [Unwind] (putStack (a1 : a : as) state)
+    newState (NGlobal n c)
+      | length as < n = error $
+          concat [ "Unwinding with too few arguments. "
+                 , show n, " - ", show c ]
+      | otherwise     = putCode c (putStack (rearrange n heap stack) state)
 
-        rearrange :: Int -> GmHeap -> GmStack -> GmStack
-        rearrange n heap as = take n as' ++ drop n as
-          where as' = map (getArg . hLookup heap) (tail as)
+    rearrange :: Int -> GmHeap -> GmStack -> GmStack
+    rearrange n heap as = take n as' ++ drop n as
+      where as' = map (getArg . hLookup heap) (tail as)
 
-        getArg :: Node -> Addr
-        getArg (NAp _ a2) = a2
-        getArg n = error $ "getArg of " ++ show n
+    getArg :: Node -> Addr
+    getArg (NAp _ a2) = a2
+    getArg n = error $ "getArg of " ++ show n
 
 dispatch (Alloc n) state = putStack (new_addrs ++ stack) (putHeap new_heap state)
   where
@@ -267,6 +281,70 @@ dispatch (Alloc n) state = putStack (new_addrs ++ stack) (putHeap new_heap state
 dispatch (Slide n) state = putStack (a : drop n as) state
   where (a : as) = getStack state
 
+dispatch Eval state = putCode [Unwind] (putStack [a] (putDump ((code, as) : dump) state))
+  where
+    (a : as) = getStack state
+    code = getCode state
+    dump = getDump state
+
+dispatch Add state = arithmetic2 (+) state
+dispatch Sub state = arithmetic2 (-) state
+dispatch Mul state = arithmetic2 (*) state
+dispatch Div state = arithmetic2 div state
+dispatch Neg state = arithmetic1 (\i -> (-i)) state
+dispatch Eq state = comparison (==) state
+dispatch Ne state = comparison (/=) state
+dispatch Lt state = comparison (<) state
+dispatch Le state = comparison (<=) state
+dispatch Gt state = comparison (>) state
+dispatch Ge state = comparison (>=) state
+
+dispatch (Cond t f) state = putStack as (putCode ((if a == 0 then f else t) ++ i) state)
+  where
+    i = getCode state
+    (a : as) = getStack state
+
+
+boxInteger :: Int -> GmState -> GmState
+boxInteger n state = putStack (a : getStack state) (putHeap h' state)
+  where
+    (h', a) = hAlloc (getHeap state) (NNum n)
+
+boxBoolean :: Bool -> GmState -> GmState
+boxBoolean b state = putStack (a : getStack state) (putHeap h' state)
+  where
+    (h', a) = hAlloc (getHeap state) (NNum (if b then 1 else 0))
+
+unboxInteger :: Addr -> GmState -> Int
+unboxInteger a state = ub (hLookup (getHeap state) a)
+  where
+    ub (NNum i) = i
+    ub _        = error "Unboxing a non-integer"
+
+primitive1 :: (b -> GmState -> GmState) -- boxing function
+           -> (Addr -> GmState -> a)    -- unboxing function
+           -> (a -> b)                  -- operator
+           -> (GmState -> GmState)      -- state transition
+primitive1 box unbox op state = box (op (unbox a state)) (putStack as state)
+  where
+    (a : as) = getStack state
+
+primitive2 :: (b -> GmState -> GmState) -- boxing function
+           -> (Addr -> GmState -> a)    -- unboxing function
+           -> (a -> a -> b)             -- operator
+           -> (GmState -> GmState)      -- state transition
+primitive2 box unbox op state = box (op (unbox a0 state) (unbox a1 state)) (putStack as state)
+  where
+    (a0 : a1 : as) = getStack state
+
+arithmetic1 :: (Int -> Int) -> (GmState -> GmState)
+arithmetic1 = primitive1 boxInteger unboxInteger
+
+arithmetic2 :: (Int -> Int -> Int) -> (GmState -> GmState)
+arithmetic2 = primitive2 boxInteger unboxInteger
+
+comparison :: (Int -> Int -> Bool) -> GmState -> GmState
+comparison = primitive2 boxBoolean unboxInteger
 
 --
 -- Printer
@@ -298,7 +376,8 @@ showInstruction = iStr . show
 
 showState :: GmState -> Iseq
 showState s = iConcat
-    [ showStack s, iNewline,
+    [ showStack s,                  iNewline,
+      showDump s,                   iNewline,
       showInstructions (getCode s), iNewline ]
 
 showStack :: GmState -> Iseq
@@ -320,6 +399,31 @@ showNode s a (NGlobal ar _) = iConcat [ iStr "Global ", iStr v, iStr "[", iNum a
 showNode _ _ (NAp a1 a2) = iConcat [ iStr "Ap ", iStr (showaddr a1),
                                      iStr " ", iStr (showaddr a2) ]
 showNode _ _ (NInd n) = iConcat [ iStr "#", iNum n ]
+
+showDump :: GmState -> Iseq
+showDump s = iConcat
+    [ iStr "  Dump:[",
+      iIndent (iInterleave iNewline (map showDumpItem (reverse (getDump s)))),
+      iStr "]" ]
+
+showDumpItem :: GmDumpItem -> Iseq
+showDumpItem (code, stack) = iConcat
+    [ iStr "<",
+      shortShowInstructions 3 code, iStr ", ",
+      shortShowStack stack,         iStr ">" ]
+
+shortShowInstructions :: Int -> GmCode -> Iseq
+shortShowInstructions number code = iConcat
+    [ iStr "{", iInterleave (iStr "; ") dotcodes, iStr "}" ]
+  where
+    codes = map showInstruction (take number code)
+    dotcodes
+      | length code > number = codes ++ [iStr "..."]
+      | otherwise            = codes
+
+shortShowStack :: GmStack -> Iseq
+shortShowStack stack = iConcat
+    [ iStr "[", iInterleave (iStr ", ") (map (iStr . showaddr) stack), iStr "]" ]
 
 showStats :: GmState -> Iseq
 showStats s = iConcat [ iStr "Steps taken = ", iNum (statGetSteps (getStats s)), iNewline ]
--- src/Programs.hs
+++ src/Programs.hs
@@ -1,6 +1,5 @@
 module Programs where
 
-pgm1, pgm2, pgm3, pgm4, pgm5, pgm6, pgm7, pgm8 :: String
 pgm1 = "main = I 3"
 pgm2 = "id = S K K ; main = id 3"
 pgm3 = "id = S K K ; main = twice twice twice id 3"
@@ -22,10 +21,33 @@ pgm8 = unlines
   , "          in k (k x) ;"
   , "main = oct I 4"
   ]
+pgm9 = "main = (4 * 5) + (2 - 5)"
+pgm10 = "inc x = x + 1 ; main = twice twice inc 4"
+pgm11 = unlines $
+  [ "cons a b cc cn = cc a b ;"
+  , "nil      cc cn = cn ;"
+  , "hd list = list K abort ;"
+  , "tl list = list K1 abort ;"
+  , "abort = abort ;"
+  , "infinite x = cons x (infinite x) ;"
+  ] ++
+  [ "length xs = xs length1 0 ;"
+  , "length1 x xs = 1 + (length xs) ;"
+  , "main = length (cons 3 (cons 3 (cons 3 nil)))"
+  ]
+pgm12 = unlines
+  [ "fac n = if (n == 0) 1 (n * fac (n-1)) ;"
+  , "main = fac 5"
+  ]
+pgm13 = unlines
+  [ "gcd a b = if (a == b) a (if (a < b) (gcd b a) (gcd b (a - b))) ;"
+  , "main = gcd 6 10"
+  ]
+pgm14 = unlines
+  [ "nfib n = if (n == 0) 1 (1 + ((nfib (n-1)) + (nfib (n-2)))) ;"
+  , "main = nfib 4"
+  ]
 
-
-
-testPgm :: String
 testPgm = unlines
   [ "pair x y f = f x y ;"
   , "fst p = p K ;"
